edac6ad56b6310be76211da1f2b63892bce6c726
ETG1 <elijahkazungu22@gmail.com>
2023-08-11 16:31:15 -0700

    Python - import & modules

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Print's the sum, difference, multiple and quotient of 10 and 5."""
+    from calculator_1 import addittion, subtraction, multiplication, division
+
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, addition(a, b)))
+    print("{} - {} = {}".format(a, b, subtraction(a, b)))
+    print("{} * {} = {}".format(a, b, multiplication(a, b)))
+    print("{} / {} = {}".format(a, b, division(a, b)))
+
---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+def addition(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+
+def subtraction(a, b):
+    """My subtraction function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
+
+
+def multiplication(a, b):
+    """My multiplication function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
+
+
+def division(a, b):
+    """My division function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b)
+
