da895537fd24d14fe5f8d59257d9d25f9068c4e5
ETG1 <elijahkazungu22@gmail.com>
2023-09-05 16:35:22 -0700

    Test driven development(MDTs)

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Defines an integer addition function.'''
+
+def add_integer(a, b=98):
+    '''Function adds two integer and/or float numbers
+    Args:
+        a: first number
+        b: second number
+    Returns:
+        Thaddition of the two given numbers
+    Raises:
+        TypeError: If a or b aren't integer or float numbers
+    '''
+
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return (a + b)
