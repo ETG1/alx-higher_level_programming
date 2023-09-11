a363ad56da1a6ca9d96b189d7bcc3cf3bbd4d373
ETG1 <elijahkazungu22@gmail.com>
2023-09-11 15:38:12 -0700

    Python Inheritance(MDTs)

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,7 +5,7 @@
 def is_same_class(obj, a_class):
     '''Returns true if object is an instance of the
     class, otherwise return false.'''
-    
+
     if type(obj) == a_class:
         return True
     return False
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 def inherits_from(obj, a_class):
     '''Returns true if object is an instance of a class that inherited
     (directly or indirectly) from the specified class; otherwise False.'''
-    
+
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
