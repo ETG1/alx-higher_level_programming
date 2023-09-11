6058a0ea277199a553fdcdfe663993a03d07d420
ETG1 <elijahkazungu22@gmail.com>
2023-09-11 15:42:28 -0700

    Python Inheritance(MDTs)

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''Defines inherited class-checking function.'''
 
+
 def inherits_from(obj, a_class):
     '''Returns true if object is an instance of a class that inherited
     (directly or indirectly) from the specified class; otherwise False.'''
