41142c13262d10a3197c7a712f1cccdb12ce4d00
ETG1 <elijahkazungu22@gmail.com>
2023-10-18 14:49:38 -0700

    SQL - More queries(MDTsks)

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- Script lists all cities contained in the database hbtn_0d_usa.
+-- Each record displays: cities.id - cities.name - states.name.
+-- Results are sorted in ascending order by cities.id.
+
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id;
